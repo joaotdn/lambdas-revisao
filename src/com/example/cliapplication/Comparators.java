6b57f3b1d6a6d9263ef9412c9c759d6ab5d6950e
João Teodoro <joaotdn@gmail.com>
2018-10-15 03:39:52 -0300

    revisando comparators

--- src/com/example/cliapplication/Comparators.java
+++ src/com/example/cliapplication/Comparators.java
@@ -3,10 +3,11 @@ package com.example.cliapplication;
 import com.example.cliapplication.entidades.Usuario;
 import com.example.cliapplication.servicos.UsuarioServico;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.comparing;
 
 public class Comparators {
 
@@ -31,12 +32,19 @@ public class Comparators {
 //        que recebe um comparator como argumento
 //        usuarios.sort((u1, u2) -> u1.getNome().compareTo(u2.getNome()));
 //        utilizando o metodo estatico da interface comparator
-        Comparator<Usuario> comparator = Comparator.comparing(u -> u.getNome());
-        usuarios.forEach(u -> System.out.println(u.getNome()));
-        System.out.println("=======");
-        usuarios.sort(comparator);
-        usuarios.forEach(u -> System.out.println(u.getNome()));
+//        Comparator<Usuario> comparator = Comparator.comparing(u -> u.getNome());
+//        usuarios.forEach(u -> System.out.println(u.getNome()));
+//        System.out.println("=======");
+//        usuarios.sort(comparator);
+//        usuarios.sort(comparing(Usuario::getNome));
+//        usuarios.forEach(u -> System.out.println(u.getNome()));
 
+        List<String> list = Arrays.asList("Joao", "Zelia", "Carlos", "Maria", "José");
+        list.sort(Comparator.naturalOrder());
+        list.forEach(System.out::println);
+        System.out.println("-----------------");
+        list.sort(Comparator.reverseOrder());
+        list.forEach(System.out::println);
 
     }
 
